8a6752ab8280a0bd1e72c66e5bdc7020c89c31e0
mcclurejt <mcclurejt@gmail.com>
2022-12-12 22:23:00 -0600

    use appropriate block time and round

--- egps/helpers/expiry.ts
+++ egps/helpers/expiry.ts
@@ -2,8 +2,8 @@ import { ethers } from 'ethers'
 
 export async function getExpiry(provider: ethers.providers.Provider, daysOpen: number = 7) {
   const secondsPerDay = 60 * 60 * 24 // seconds per minute * minutes per hour * hours per day
-  const secondsPerBlock = 12 // eth mainnet
-  const blocksPerDay = secondsPerDay / secondsPerBlock
+  const secondsPerBlock = 13.3 // eth mainnet
+  const blocksPerDay = Math.round(secondsPerDay / secondsPerBlock)
   const latestBlock = await provider.getBlock('latest')
   return latestBlock.number + blocksPerDay * daysOpen
 }
