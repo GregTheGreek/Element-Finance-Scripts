0c935026e986e2ee19b07c8b2d4615b5e80e222d
mcclurejt <mcclurejt@gmail.com>
2023-03-14 01:37:18 -0500

    install hardhat + remove typechain and artifacts from version control

---
+++ hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.18',
+  paths: {
+    sources: './egps',
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+      },
+      accounts: {
+        accountsBalance: '100000000000000000000000', // 100000 ETH
+        count: 5,
+      },
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+    },
+  },
+  typechain: {
+    outDir: 'typechain',
+  },
+}
+
+export default config
