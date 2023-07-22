226de6e90733fd54f86caeff50a531418f22d948
William Villanueva <wvillanu89@gmail.com>
2023-07-22 17:04:24 +0800

    Update withdraw tokens in config from balancer pools

--- egps/egp-16/egp-16.ts
+++ egps/egp-16/egp-16.ts
@@ -126,111 +126,199 @@ export const balancerPools = [
         // LPePyvCurve-MIM-11FEB22
         pool: '0x09b1b33BaD0e87454ff05696b1151BFbD208a43F',
         poolId: '0x09b1b33bad0e87454ff05696b1151bfbd208a43f0002000000000000000000a6',
+        withdraw: {
+            pt: '0x418De6227499181B045CAdf554030722E460881a',
+            base: '0x5a6A4D54456819380173272A5E8E9B9904BdF41B',
+        }
     },
     {
         // LPePyvCurve-EURS-11FEB22
         pool: '0x6AC02eCD0c2A23B11f9AFb3b3Aaf237169475cac',
         poolId: '0x6ac02ecd0c2a23b11f9afb3b3aaf237169475cac0002000000000000000000a8',
+        withdraw: {
+            pt: '0x2A8f5649DE50462fF9699Ccc75A2Fb0b53447503',
+            base: '0x194eBd173F6cDacE046C53eACcE9B953F28411d1',
+        }
     },
     {
         // LPePyvcrvSTETH-15APR22
         pool: '0xb03C6B351A283bc1Cd26b9cf6d7B0c4556013bDb',
         poolId: '0xb03c6b351a283bc1cd26b9cf6d7b0c4556013bdb0002000000000000000000ab',
+        withdraw: {
+            pt: '0x2361102893CCabFb543bc55AC4cC8d6d0824A67E',
+            base: '0x06325440D014e39736583c165C2963BA99fAf14E',
+        }
     },
     {
         // LPePyvUSDC-17DEC21
         pool: '0x90ca5cef5b29342b229fb8ae2db5d8f4f894d652',
-        poolId: '0x90ca5cef5b29342b229fb8ae2db5d8f4f894d6520002000000000000000000b5'
+        poolId: '0x90ca5cef5b29342b229fb8ae2db5d8f4f894d6520002000000000000000000b5',
+        withdraw: {
+            pt: '0x76a34D72b9CF97d972fB0e390eB053A37F211c74',
+            base: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        }
     },
     {
         // LPePyvCurveLUSD-28SEP21
         pool: '0xA8D4433BAdAa1A35506804B43657B0694deA928d',
         poolId: '0xa8d4433badaa1a35506804b43657b0694dea928d00020000000000000000005e',
+        withdraw: {
+            pt: '0x9b44Ed798a10Df31dee52C5256Dcb4754BCf097E',
+            base: '0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA',
+        }
     },
     {
         // LPePyvcrv3crypto-29APR22
         pool: '0x6Dd0F7c8F4793ed2531c0df4fEA8633a21fDcFf4',
         poolId: '0x6dd0f7c8f4793ed2531c0df4fea8633a21fdcff40002000000000000000000b7',
+        withdraw: {
+            pt: '0x285328906D0D33cb757c1E471F5e2176683247c2',
+            base: '0xc4AD29ba4B3c580e6D59105FFf484999997675Ff',
+        }
     },
     {
         // LPePyvCurveLUSD-27DEC21
         pool: '0x893B30574BF183d69413717f30b17062eC9DFD8b',
         poolId: '0x893b30574bf183d69413717f30b17062ec9dfd8b000200000000000000000061',
+        withdraw: {
+            pt: '0xa2b3d083AA1eaa8453BfB477f062A208Ed85cBBF',
+            base: '0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA',
+        }
     },
     {
         // LPePyvUSDC-29APR22
         pool: '0x7edde0cb05ed19e03a9a47cd5e53fc57fde1c80c',
         poolId: '0x7edde0cb05ed19e03a9a47cd5e53fc57fde1c80c0002000000000000000000c8',
+        withdraw: {
+            pt: '0x52C9886d5D87B0f06EbACBEff750B5Ffad5d17d9',
+            base: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        }
     },
     {
         // LPePyvCurve-MIM-29APR22
         pool: '0x14792d3F6FcF2661795d1E08ef818bf612708BbF',
         poolId: '0x14792d3f6fcf2661795d1e08ef818bf612708bbf0002000000000000000000be',
+        withdraw: {
+            pt: '0xC63958D9D01eFA6B8266b1df3862c6323CbDb52B',
+            base: '0x5a6A4D54456819380173272A5E8E9B9904BdF41B',
+        }
     },
     {
         // LPePyvCrvTriCrypto-15AUG21
         pool: '0x3A693EB97b500008d4Bb6258906f7Bbca1D09Cc5',
         poolId: '0x3a693eb97b500008d4bb6258906f7bbca1d09cc5000200000000000000000065',
+        withdraw: {
+            pt: '0x237535Da7e2f0aBa1b68262ABCf7C4e60B42600C',
+            base: '0xcA3d75aC011BF5aD07a98d02f18225F9bD9A6BDF',
+        }
     },
     {
         // LPePyvCurve-alUSD-29APR22
         pool: '0x63E9B50DD3eB63BfBF93B26F57b9EFB574e59576',
         poolId: '0x63e9b50dd3eb63bfbf93b26f57b9efb574e595760002000000000000000000cf',
+        withdraw: {
+            pt: '0xEaa1cBA8CC3CF01a92E9E853E90277B5B8A23e07',
+            base: '0x43b4FdFD4Ff969587185cDB6f0BD875c5Fc83f8c',
+        }
     },
     {
         // LPePyvcrvSTETH-15OCT21
         pool: '0xce16E7ed7654a3453E8FaF748f2c82E57069278f',
         poolId: '0xce16e7ed7654a3453e8faf748f2c82e57069278f00020000000000000000006d',
+        withdraw: {
+            pt: '0xEaa1cBA8CC3CF01a92E9E853E90277B5B8A23e07',
+            base: '0x06325440D014e39736583c165C2963BA99fAf14E',
+        }
     },
     {
         // LPePyvWBTC-29APR22
         pool: '0x4bd6D86dEBdB9F5413e631Ad386c4427DC9D01B2',
         poolId: '0x4bd6d86debdb9f5413e631ad386c4427dc9d01b20002000000000000000000ec',
+        withdraw: {
+            pt: '0x49e9e169f0B661Ea0A883f490564F4CC275123Ed',
+            base: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+        }
     },
     {
         // LPePyvDAI-16OCT21
         pool: '0x71628c66C502F988Fbb9e17081F2bD14e361FAF4',
         poolId: '0x71628c66c502f988fbb9e17081f2bd14e361faf4000200000000000000000078',
+        withdraw: {
+            pt: '0xb1cc77e701de60FE246607565CF7eDC9D9b6b963',
+            base: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+        }
     },
     {
         // LPePyvCurveLUSD-29APR22
         pool: '0x56f30398d13f111401d6e7ffe758254a0946687d',
         poolId: '0x56f30398d13f111401d6e7ffe758254a0946687d000200000000000000000105',
+        withdraw: {
+            pt: '0x0740A6CfB9468B8b53070C0B327099293DCCB82d',
+            base: '0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA'
+        }
     },
     {
         // LPePyvUSDC-29OCT21
         pool: '0x787546Bf2c05e3e19e2b6BDE57A203da7f682efF',
         poolId: '0x787546bf2c05e3e19e2b6bde57a203da7f682eff00020000000000000000007c',
+        withdraw: {
+            pt: '0xf38c3E836Be9cD35072055Ff6a9Ba570e0B70797',
+            base: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+        }
     },
     {
         // LPePyvDAI-29APR22
         pool: '0xEdf085f65b4F6c155e13155502Ef925c9a756003',
         poolId: '0xedf085f65b4f6c155e13155502ef925c9a756003000200000000000000000123',
+        withdraw: {
+            pt: '0x2c72692E94E757679289aC85d3556b2c0f717E0E',
+            base: '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+        }
     },
     {
         // LPePyvcrv3crypto-12NOV21
         pool: '0xF6dc4640D2783654BeF88E0dF3fb0F051f0DfC1A',
         poolId: '0xf6dc4640d2783654bef88e0df3fb0f051f0dfc1a00020000000000000000007e',
+        withdraw: {
+            pt: '0x9CF2AB51aC93711Ec2fa32Ec861349568A16c729',
+            base: '0xc4AD29ba4B3c580e6D59105FFf484999997675Ff'
+        }
     },
     {
         // LPePyvWBTC-26NOV21
         pool: '0x4Db9024fc9F477134e00Da0DA3c77DE98d9836aC',
         poolId: '0x4db9024fc9f477134e00da0da3c77de98d9836ac000200000000000000000086',
+        withdraw: {
+            pt: '0x6BF924137E769C0A5c443dcE6eC885552d31D579',
+            base: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+        }
     },
     {
         // LPePyvUSDC-28JAN22
         pool: '0x10a2F8bd81Ee2898D7eD18fb8f114034a549FA59',
         poolId: '0x10a2f8bd81ee2898d7ed18fb8f114034a549fa59000200000000000000000090',
+        withdraw: {
+            pt: '0x8a2228705ec979961F0e16df311dEbcf097A2766',
+            base: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+        }
     },
     {
         // LPePyvDAI-28JAN22
         pool: '0xA47D1251CF21AD42685Cc6B8B3a186a73Dbd06cf',
         poolId: '0xa47d1251cf21ad42685cc6b8b3a186a73dbd06cf000200000000000000000097',
+        withdraw: {
+            pt: '0x449D7C2e096E9f867339078535b15440d42F78E8',
+            base: '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+        }
     },
     {
         // LPePyvcrvSTETH-28JAN22
         pool: '0x544c823194218f0640daE8291c1f59752d25faE3',
         poolId: '0x544c823194218f0640dae8291c1f59752d25fae3000200000000000000000093',
+        withdraw: {
+            pt: '0x720465A4AE6547348056885060EEB51F9CAdb571',
+            base: '0x06325440D014e39736583c165C2963BA99fAf14E'
+        }
     },
 ];
 
